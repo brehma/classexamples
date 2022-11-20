b65894803752c1a04fae2d0739977512a87526a2
Adam Brehm <brehma@users.noreply.github.com>
2022-11-19 16:05:40 -0800

    Update bullseye.js

--- bullseye/bullseye.js
+++ bullseye/bullseye.js
@@ -1,20 +1,16 @@
+var multiplier = prompt("choose a value between 1-10"); // Type of input + user prompt
 
-var multiplier;
-var multiplier = prompt("choose a value between 1-10") // Type of input + user prompt
+// Set counter to 0
+var i = 0;
 
-//var operator = 'multiplication'; // Type of calculation
-var i = 0;                // Set counter to 0
-var msg = '<h2> Multiplication Table </h2>';              // Message
-
-/*if (operator === 'addition') {
-} else {
-  // Do multiplication */
-  while (i <= 11) {
-    msg += i + ' x ' + multiplier + ' = ' + (i * multiplier) + '<br />';
-    i++;
-//  }
-//}
+// Show Multiplication Table As A Message
+var msg = '<h2> Multiplication Table </h2>';
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
+
+while (i <= 11) {
+    msg += i + ' x ' + multiplier + ' = ' + (i * multiplier) + '<br />';
+    i++;
+}
